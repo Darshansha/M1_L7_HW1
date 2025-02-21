9e352a4b4b341950e942b147d54b378128d45e28
Darshansha <darshansha@gmail.com>
2025-02-21 22:38:32 +0000

    commit1

---
+++ main1.py
@@ -0,0 +1,8 @@
+
+char = input("Enter a character: ")
+
+if len(char) != 1:
+    print("Please enter exactly one character.")
+else:
+    ascii_value = ord(char)
+    print("The ASCII value of ", char, " is", ascii_value)
